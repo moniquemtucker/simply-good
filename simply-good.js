4772d6bb377eb6f532d03682aa2a25e4dece4a17
moniquemtucker <moniquetucker@gmail.com>
2014-10-30 10:08:02 -0700

    simply good. adding js for diary.

--- simply-good.js
+++ simply-good.js
@@ -1,3 +1,18 @@
 /**
  * Created by student on 10/24/14.
  */
+$(document).ready(function() {
+    $(".add-portion-veg").click( function(){
+           $("#diary-veg").append("<li><span class='ionicons ion-ios7-tennisball-outline'></span></li>");
+    });
+    $(".add-portion-pro").click( function(){
+           $("#diary-pro").append("<li><span class='ionicons ion-ios7-tennisball-outline'></span></li>");
+    });
+    $(".add-portion-fru").click( function(){
+           $("#diary-fru").append("<li><span class='ionicons ion-ios7-tennisball-outline'></span></li>");
+    });
+    $(".add-portion-other").click( function(){
+           $("#diary-other").append("<li><span class='ionicons ion-ios7-tennisball-outline'></span></li>");
+    });
+//add a counter and if statement to format; pop ups for goals achieved
+});
