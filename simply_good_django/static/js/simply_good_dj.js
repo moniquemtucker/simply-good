d6522fe378c11f321f09a75caf258824bb4e6eba
moniquemtucker <moniquetucker@gmail.com>
2014-11-05 10:35:50 -0800

    simply good django files. creating registration error template

--- simply_good_django/static/js/simply_good_dj.js
+++ simply_good_django/static/js/simply_good_dj.js
@@ -1,6 +1,15 @@
 /**
  * Created by student on 10/30/14.
  */
+    $(window).load(function(){
+        $('#register-modal').modal('show');
+    });
+
+    $(window).load(function(){
+        $('#login-modal').modal('show');
+    });
+
+
 $(document).ready(function() {
 
 //    $("#home-signup-pop").dialog({ autoOpen: false });
